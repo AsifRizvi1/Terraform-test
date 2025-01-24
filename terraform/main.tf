a5698d622f3105e20a5b4fee422532627b53cf1c
Ubuntu <ubuntu@ip-172-31-6-83.us-east-2.compute.internal>
2025-01-24 17:07:41 +0000

    Add terraform CI/CD for EC2

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+
+# Define the provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Define an AWS EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0cb91c7de36eed2cb" # Example AMI ID for us-east-2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+# Output the public IP of the instance
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the EC2 instance"
+}
